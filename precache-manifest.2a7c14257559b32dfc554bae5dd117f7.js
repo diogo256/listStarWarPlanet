52e147a0a875f06d02bf35ca3aa9c800e2ba63ca
Diogo Oliveira <diogo.oliveira@mjv.com.br>
2019-07-03 01:07:52 -0300

    Updates

---
+++ precache-manifest.2a7c14257559b32dfc554bae5dd117f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d0be58ce90e7ac36efb62619f2f699",
+    "url": "/listStarWarPlanet/index.html"
+  },
+  {
+    "revision": "7e5d4fd8a4d32ccf10e6",
+    "url": "/listStarWarPlanet/static/css/main.ce9840e3.chunk.css"
+  },
+  {
+    "revision": "18348c246e7e6d5c68d9",
+    "url": "/listStarWarPlanet/static/js/2.fd29439f.chunk.js"
+  },
+  {
+    "revision": "7e5d4fd8a4d32ccf10e6",
+    "url": "/listStarWarPlanet/static/js/main.cfee611d.chunk.js"
+  },
+  {
+    "revision": "d39cf6d50eb40b06d115",
+    "url": "/listStarWarPlanet/static/js/runtime~main.f6bbe4ca.js"
+  }
+]);
